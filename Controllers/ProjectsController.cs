af8f954c17018281c93c573304d8fe43c6e405b5
Mateus Souza <msouzacrv@gmail.com>
2024-01-24 19:49:35 -0300

    feat: Creating the controllers base

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orange_Portfolio_BackEnd.Models;
+using Orange_Portfolio_BackEnd.Models.Interfaces;
+
+namespace Orange_Portfolio_BackEnd.Controllers
+{
+    [Route("api/v1/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public ProjectsController(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var listProjects = _projectRepository.GetAll();
+            return Ok(listProjects);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var project = _projectRepository.GetById(id);
+            return project == null ? NotFound("Project not found!") : Ok(project);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Project project)
+        {
+            _projectRepository.Add(project);
+            return Ok(project);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] Project project, int id)
+        {
+            _projectRepository.Update(project);
+            return Ok(project);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _projectRepository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orange_Portfolio_BackEnd.Models;
+using Orange_Portfolio_BackEnd.Models.Interfaces;
+
+namespace Orange_Portfolio_BackEnd.Controllers
+{
+    [Route("api/v1/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var listUsers = _userRepository.GetAll();
+            return Ok(listUsers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _userRepository.GetById(id);
+            return user == null ? NotFound("User not found!") : Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] User user)
+        {
+            _userRepository.Add(user);
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] User user, int id)
+        {
+            _userRepository.Update(user);
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userRepository.Delete(id);
+            return Ok();
+        }
+    }
+}
