3b6a9b2c6cb1bca314fcb0cd31bfbb8bde60c2aa
Mateus Souza <msouzacrv@gmail.com>
2024-01-24 15:00:48 -0300

    feat: Mapping Model Project

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,16 +1,39 @@
-﻿namespace Orange_Portfolio_BackEnd.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Orange_Portfolio_BackEnd.Models
 {
     public class Project
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Image é obrigatório.")]
+        [StringLength(45)]
         public string Title { get; set; }
+
+        [Url]
+        [MaxLength(255)]
         public string Link { get; set; }
-        public string Description { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string? Description { get; set; }
+
+        
+        [Required]
+        [MaxLength(255)]
+        [DataType(DataType.Upload)]
         public string image { get; set; }
+
+        [DataType(DataType.Date)]
         public DateOnly UploadDate { get; set; }
 
+        [Required]
+        [ForeignKey("User")]
         public int UserId { get; set; }
         public User User { get; set; }
+
+        [InverseProperty("Projects")]
         public ICollection<Tag> Tags { get; set; }
     }
 }
